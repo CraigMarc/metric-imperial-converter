fa514e632ffcc8b2c01cbd51d75e2f762d9a0e0b
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-05-14 19:44:11 -0400

    Create api.js

---
+++ api.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert')
+  .get(async (req, res) => {
+  try {
+
+console.log(req.query.input)
+let input = req.query.input
+let initNum = convertHandler.getNum(input);
+let initUnit = convertHandler.getUnit(input);
+let returnUnit = convertHandler.getReturnUnit(initUnit);
+let spellout = convertHandler.spellOutUnit(returnUnit);
+let returnNum = convertHandler.convert(initNum, initUnit);
+let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    console.log(initNum + "initNum")
+    
+    console.log(returnUnit + "returnUnit")
+    console.log(spellout)
+    console.log(returnNum + "returnNum")
+console.log(initUnit + "initunit")
+
+if (initUnit == "invalid unit" && initNum == "invalid number") {
+  return res.json('invalid number and unit')
+}
+    
+if (initUnit == "invalid unit") {
+  return res.json("invalid unit")
+}
+if (initNum == "invalid number") {
+  return res.json("invalid number")
+}
+
+    
+ let response = {
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string
+      }
+      return res.json(response);
+
+
+    }
+  catch (err) {
+    console.log(err);
+  }
+
+});
+
+};
